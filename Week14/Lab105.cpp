fb6db983a5b61066e46daa32cdf2dd5b4ffc5cb8
Kayw Lee <kwooler@gmail.com>
2023-04-27 21:19:35 -0700

    Apr 26

--- Week14/Lab105.cpp
+++ Week14/Lab105.cpp
@@ -3,21 +3,55 @@
 #include <string>
 using namespace std;
 
-// int splitwords(string, char);
-// int findnumchars(string);
+int splitwords(string, char);
+int findnumchars(string);
 
 int main()
 {
 	string txt1("ABCDEF,GHI,JKLMN,OP");
 	string txt2("BACDGABCDAZ");
-	int result;
 	char delimiter = ',';
+	int result;
 
-	result = txt1.find(',');
-	string sub = txt1.substr(result + 1, 3);
-	cout << result << endl;
+	result = splitwords(txt1, delimiter);
+	cout << "The number of substring " << result << endl;
+
+	result = findnumchars(txt2);
+	cout << "The number of unique letters " << result << endl;
+}
+
+int findnumchars(string txt)
+{
+	int seen[26] = {0};
+	int index, cnt = 0;
+	string::iterator iter;
+	for (iter = txt.begin(); iter != txt.end(); iter++)
+	{
+		index = *iter - 'A';
+		seen[index] += 1;
+	}
+	for (int i = 0; i < 26; i++)
+	{
+		if (seen[i] == 1)
+			cnt += 1;
+	}
+	return cnt;
+}
+
+int splitwords(string txt1, char delimiter)
+{
+	int start, end, cnt = 0;
+	string sub;
+	start = 0;
+	while ((end = txt1.find(delimiter, start)) != string::npos)
+	{
+		sub = txt1.substr(start, end - start);
+		start = end + 1;
+		cout << sub << endl;
+		cnt += 1;
+	}
+	sub = txt1.substr(start, txt1.size() - start);
+	cnt += 1;
 	cout << sub << endl;
-
-	//     result = splitwords(txt1, delimiter);
-	//     result = findnumchars(txt2);
+	return cnt;
 }
